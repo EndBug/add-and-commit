ba21c1c6dd3b5d3995bda03ad27afd26c10bce44
Federico Grandi <fgrandi30@gmail.com>
2019-12-10 22:49:44 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as path from 'path'
-import { spawn } from 'child_process';
+import { spawn, execFileSync } from 'child_process';
 
 try {
-  spawn('ls')
-  spawn(path.join(__dirname, '../src/entrypoint.sh'))
+  execFileSync(path.join(__dirname, '../src/entrypoint.sh'))
 } catch (e) {
   core.setFailed(e)
 }
