aba29c32b040593e45907324b0308556ac6bf545
Federico Grandi <fgrandi30@gmail.com>
2019-12-10 22:22:33 +0100

    Avoid @action/exec

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
 import * as path from 'path'
+import { exec } from 'child_process';
 
 try {
-  exec.exec('ls')
-  exec.exec(`../src/entrypoint.sh`).catch(core.setFailed);
+  exec('ls')
+  exec(`../src/entrypoint.sh`)
 } catch (e) {
   core.setFailed(e)
 }
