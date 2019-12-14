34bdaa49e26c13276439759bbaeb36dffabd978f
Federico Grandi <fgrandi30@gmail.com>
2019-12-14 21:24:15 +0100

    Copy scripts to local dir

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import * as core from '@actions/core'
 import * as shell from 'shelljs'
-import * as path from 'path'
 
 try {
-  shell.exec(path.join(__dirname, '../src/entrypoint.sh'))
+  shell.exec('./entrypoint.sh')
 } catch (e) {
   core.setFailed(e)
 }
