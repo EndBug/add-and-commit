0ca607813c93103c96806daf99fdc52a4d2a5cfc
Federico Grandi <fgrandi30@gmail.com>
2019-12-14 21:14:50 +0100

    Try with shelljs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
+import * as shell from 'shelljs'
 import * as path from 'path'
-import { spawn, execFileSync } from 'child_process';
 
 try {
-  execFileSync(path.join(__dirname, '../src/entrypoint.sh'))
+  shell.exec(path.join(__dirname, '../src/entrypoint.sh'))
 } catch (e) {
   core.setFailed(e)
 }
