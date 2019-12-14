292587718277e296f2d4f19517f2631540ccef28
Federico Grandi <fgrandi30@gmail.com>
2019-12-14 21:28:45 +0100

    Still use path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core'
 import * as shell from 'shelljs'
+import * as path from 'path'
 
 try {
-  shell.exec('./entrypoint.sh')
+  shell.exec(path.join(__dirname, './entrypoint.sh'))
 } catch (e) {
   core.setFailed(e)
 }
