9eebf45edd84896540cc189e010434fb46804aba
Federico Grandi <fgrandi30@gmail.com>
2020-08-22 20:05:36 +0200

    chore: commit local work

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,83 @@
-import { info, setFailed, getInput as getInputCore, warning, debug, startGroup } from '@actions/core'
+import { info, setFailed, getInput as getInputCore, warning, debug, startGroup, endGroup } from '@actions/core'
 import fs from 'fs'
 import axios from 'axios'
-import simpleGit from 'simple-git'
+import simpleGit, { Response } from 'simple-git'
 
-import { Input, inputs } from './inputs'
-type InputRecord = Record<Input, string | boolean | undefined>
+import { Input } from './inputs'
 
 const git = simpleGit({
   baseDir: getInput('cwd'),
 
 });
 
-// let inputValues: InputRecord
-
 (async () => {
   await checkInputs()
-  // inputValues = inputs.reduce((obj, input) => ({ ...obj, [input]: getInput(input) }), {}) as InputRecord
 
   startGroup('Internal logs')
-  await setLoginInfo()
+  info('Staging files...')
+  await add(false)
+  await remove(false)
 
-  await git.fetch()
+  info('Checking for uncommitted changes in the git working tree...')
+  const changedFiles = (await git.diffSummary(['--cached'])).files.length
+  if (changedFiles > 0) {
+    info(`Found ${changedFiles} changed files.`)
 
-  info('Switching/creating branch...')
-  await git
-    .checkout(getInput('ref'))
-    .catch(() => git.checkoutLocalBranch(getInput('ref')))
+    await setLoginInfo()
 
-  info('Pulling from remote...')
-  await git
-    .fetch()
-    .pull()
+    await git.fetch()
 
-  info('Resetting files...')
-  await git.reset()
+    info('Switching/creating branch...')
+    await git
+      .checkout(getInput('branch'))
+      .catch(() => git.checkoutLocalBranch(getInput('branch')))
 
-  if (getInput('add')) {
-    info('Adding files...')
+    info('Pulling from remote...')
+    await git
+      .fetch()
+      .pull()
 
-    if (git.checkIgnore())
-      await git.add('')
+    info('Resetting files...')
+    await git.reset()
+
+    if (getInput('add')) {
+      info('Adding files...')
+      await add()
+    } else info('No files to add.')
+
+    if (getInput('remove')) {
+      info('Removing files...')
+      await remove()
+    } else info('No files to remove.')
+
+    info('Creating commit...')
+    await git.commit(getInput('message'), undefined, {
+      '--author': `"${getInput('author_name')} <${getInput('author_email')}>"`,
+      ...(getInput('signoff') ? {
+        '--signoff': null
+      } : {})
+    })
+
+    if (getInput('tag')) {
+      info('Tagging commit...')
+      await git.tag(getInput('tag').split(' '))
+    } else info('No tag info provided.')
+
+    info('Pushing commit to repo...')
+    // @ts-expect-error
+    await git.push(`--set-upstream origin "${getInput('branch')}"`.split(' '))
+
+    if (getInput('tag')) {
+      info('Pushing tags to repo...')
+      // @ts-expect-error
+      await git.push(`--set-upstream origin "${getInput('branch')}" --force --tags`.split(' '))
+    } else info('No tags to push.')
+
+    endGroup()
+    info('Task completed.')
+  } else {
+    endGroup()
+    info('Working tree clean. Nothing to commit.')
   }
 
 })().catch(setFailed)
@@ -139,4 +177,20 @@ async function setLoginInfo() {
     .addConfig('user.email', getInput('author_email'))
     .addConfig('user.name', getInput('author_name'))
   debug('> Current git config\n' + JSON.stringify((await git.listConfig()).all, null, 2))
+}
+
+function add(logWarning = true): Promise<void | Response<void>> {
+  return git.add(getInput('add').split(' ')).catch((e: Error) => {
+    if (e.message.startsWith('fatal: pathspec') && e.message.endsWith('did not match any files'))
+      logWarning && warning('Add command did not match any file.')
+    else throw e
+  })
+}
+
+function remove(logWarning = true) {
+  return git.rm(getInput('remove').split(' ')).catch((e: Error) => {
+    if (e.message.startsWith('fatal: pathspec') && e.message.endsWith('did not match any files'))
+      logWarning && warning('Remove command did not match any file.')
+    else throw e
+  })
 }
